533f72546e6b80bc0f997c60144103783107f6d1
KevinACoder <614824710@qq.com>
2018-04-29 16:35:00 +0800

    make up

--- linux/config_git.sh
+++ linux/config_git.sh
@@ -1,4 +1,31 @@
-sudo apt-get install git git-core git-gui git-doc git-svn git-cvs gitweb gitk git-email git-daemon-run git-el git-arch
+#$1: email addr
+#$2: user name
+
+#install git componet
+sudo apt-get install git git-core git-gui git-doc git-svn git-cvs$
+
+#backup old key and generate new key
 cd 
 cd ~/.ssh
-ls
+mkdir key_bkup
+cp id_rsa* key_bkup
+rm id_rsa*
+
+ssh-keygen -t rsa -C "$1"
+cat id_rsa.pub
+
+ssh -T git@github.com
+
+git config --global user.name "$2"
+git config --global user.email "$1"
+
+#create code bank
+cd ~/Work/src
+git init
+touch readme
+git add Readme #--all
+git commit -m "add readme file"
+git remote add origin https://github.com/你的github用户名/你的github仓库.git 
+
+git push origin master
+git clone https://github.com/你的github用户名/你的github仓库.git 
