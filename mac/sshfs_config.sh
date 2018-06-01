913f6c8369aae75a24ab0a349be3b13590061cce
KevinACoder <614824710@qq.com>
2018-06-01 22:34:45 +0800

    sshf config

--- mac/sshfs_config.sh
+++ mac/sshfs_config.sh
@@ -1,4 +1,4 @@
 #url: http://www.instructables.com/id/ShareEditCreate-Files-for-Your-Raspberry-Pi-From-a/
 
-sshfs pi@192.168.1.126:/home/pi/WorkSpace/ /Users/KevinLiu/work/pi/
-
+sshfs pi@192.168.1.126:/home/pi/work/ /Users/KevinLiu/work/
+umount -f pi@192.168.1.126:/home/pi/work/
