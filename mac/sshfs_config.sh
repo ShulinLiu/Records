179374e6c3d68a4a572ca6127103ca25d5d8d36c
KevinACoder <614824710@qq.com>
2018-06-10 10:13:59 +0800

    sshfs need id

--- mac/sshfs_config.sh
+++ mac/sshfs_config.sh
@@ -2,3 +2,6 @@
 
 sshfs pi@192.168.1.126:/home/pi/work/ /Users/KevinLiu/work/
 umount -f pi@192.168.1.126:/home/pi/work/
+
+#when ssh need indectiication
+sudo sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa paofu@192.168.1.126:/home/paofu/kevin  /Users/kevinliu/pi/unbuntu_mate 
