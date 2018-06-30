d113ec1bf3aef9613b3a83586f869bde9b475c0e
KevinACoder <38829162+KevinACoder@users.noreply.github.com>
2018-06-30 16:54:26 +0800

    Update setup_mysql.sh

--- linux/setup_mysql.sh
+++ linux/setup_mysql.sh
@@ -7,18 +7,31 @@ systemctl status mysql.service #check the status of mysql
 sudo systemctl start mysql #start up mysql service
 mysqladmin -p -u root version #check more information on mysql
 
+#login 
+mysql -p -u root
+
 #https://www.lanexa.net/2011/08/create-a-mysql-database-username-password-and-permissions-from-the-command-line/
 #create new user and grant privillege
 create user paofu_db;
 create database test_db;
 show databases;
+show tables; #show tables
+show columns from <table>; #show columns
 grant all on test_db.* to 'paofu_db'@'localhost' identified by '<root_password>';
 
 
-CREATE TABLE Persons (
+CREATE TABLE persons (
     PersonID int,
     LastName varchar(255),
     FirstName varchar(255),
     Address varchar(255),
     City varchar(255) 
 );
+
+INSERT INTO persons VALUES(
+   1, 'Zhu', 'Kevin', 'Woodland', 'SG'); #db/table/column key is case sensitive
+INSERT INTO persons(person_id, last_name, first_name, address, city) 
+   VALUES(3, 'Zhang', 'Gao', 'Linyi', 'SD'); #safer when table structure changed
+   
+SELECT * FROM persons WHERE city <> 'SG' AND person_id BETWEEN 1 AND 1 ORDER BY last_name DESC ;
+SELECT DISTINCT last_name FROM persons LIMIT 1 OFFSET 1; #limit range from line 1 to 1 more row
